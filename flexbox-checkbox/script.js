705892b3a2183177a59ebd7d8bb3b6d7b269164f
gabysantosw <44228131+gabysantosw@users.noreply.github.com>
2019-05-22 21:38:35 +0200

    starting out playground

--- flexbox-checkbox/script.js
+++ flexbox-checkbox/script.js
@@ -1,9 +1,1 @@
-const checkbox = document.getElementById('flex');
 
-checkbox.addEventListener('change', function(){
-  if(this.checked) {
-        console.log('check');
-    } else {
-        console.log('nope');
-    }
-});
